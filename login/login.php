295742391d5e7b3acd8d78aed4005e3784f4db33
sadia1662 <72683421+sadia1662@users.noreply.github.com>
2020-12-25 16:49:08 +0600

    login

--- login/login.php
+++ login/login.php
@@ -1,1 +1,38 @@
+<?
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "login";
+$username = filter_input(INPUT_POST, 'username');
+$password = filter_input(INPUT_POST, 'password');
+if (!empty($username)){
+	if (!empty($password)){
+		// Create connection
+		$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+		if (mysqli_connect_error()){
+			die('Connect Error ('. mysqli_connect_errno() .') '. mysqli_connect_error());
+		}
+		else{
+			$sql = "INSERT INTO logindatabase (username, password)
+			values ('$username','$password')";
+			if ($conn->query($sql)){
+				echo "New record is inserrted successfully";
+			}
+			else{
+				echo "Error : ". $sql ."<br>". $conn->error;
+			}
+			$conn->close();
+		}
 
+	}
+	else{
+		echo "Password should not be empty";
+		die();
+	}
+
+}
+else{
+	echo "Username should not be empty";
+	die();
+}
+?>
